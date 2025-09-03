6885e16db0c9fdebe8614d4778b45c4a7f01d95b
Addi071 <157271684+Addi071@users.noreply.github.com>
2025-09-03 14:44:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT
 const __dirname = path.resolve()
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin:"http://localhost:5173",
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
 }))
 app.use(express.json()); // Middleware to parse JSON bodies
