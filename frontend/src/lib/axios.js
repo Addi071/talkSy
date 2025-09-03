6f881b65b7666c50f68a7aded79e5c2a401c5b68
Addi071 <157271684+Addi071@users.noreply.github.com>
2025-09-03 08:45:42 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 export const axiosInstance = axios.create({
-    baseURL: 'https://talksy-p5kg.onrender.com/api',
+    baseURL: 'https://talksy-p5kg.onrender.com/api/',
     withCredentials: true,
 })
